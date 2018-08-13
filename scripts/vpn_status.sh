9a75cc6093f33cb02a1e5fb25f6d762937580059
Remon <r.s.oldenbeuving@gmail.com>
2018-08-13 11:54:25 +0000

    Make icons configurable

--- scripts/vpn_status.sh
+++ scripts/vpn_status.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+vpn_online_option_string="@vpn_online_icon"
+vpn_offline_option_string="@vpn_offline_icon"
+
+vpn_online_icon_default="✔"
+vpn_offline_icon_default="✖"
+
+source $CURRENT_DIR/shared.sh
+
 main() {
   if [[ $(ifconfig -s | awk '{print $1}' | grep -c tun9) -eq 1 ]]; then
-    echo "✔ VPN"
+    printf "$(get_tmux_option "$vpn_online_option_string" "$vpn_online_icon_default")"
   else
-    echo "✖ VPN"
+    printf "$(get_tmux_option "$vpn_offline_option_string" "$vpn_offline_icon_default")"
   fi
 }
 main
